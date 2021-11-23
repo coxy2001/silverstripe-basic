587050e22821ad8f234f35febcbaf65d197ce1fb
coxy2001 <64635910+coxy2001@users.noreply.github.com>
2021-11-23 15:03:51 +1300

    TinyMCE config test

--- app/_config.php
+++ app/_config.php
@@ -7,3 +7,35 @@ use SilverStripe\Security\Member;
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+
+/**
+ * HTML Editor Config setup
+ */
+
+$formats = [
+    [
+        'title' => 'Misc Styles', 'items' => [
+            [
+                'title' => 'Button',
+                'selector' => 'a',
+                'wrapper' => false,
+                'classes' => 'btn btn-primary',
+                'merge_siblings' => false,
+            ],
+            [
+                'title' => 'Phone',
+                'inline' => 'span',
+                'classes' => 'phone-link',
+                'merge_siblings' => true,
+            ]
+        ]
+    ],
+];
+
+TinyMCEConfig::get('cms')
+    ->setContentCSS(['/public/assets/_css/themes-cb-basic-scss-style.css'])
+    ->addButtonsToLine(1, 'styleselect')
+    ->setOptions([
+        'importcss_append' => true,
+        'style_formats' => $formats,
+    ]);
